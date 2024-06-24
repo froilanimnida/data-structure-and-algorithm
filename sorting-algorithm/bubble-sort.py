ff2e8669b8a81cac855333b9c9473846492aa415
froilanimnida <froilanaquino1@gmail.com>
2024-06-24 17:05:29 +0800

    added docs for previous sorting algorithm

--- sorting-algorithm/bubble-sort.py
+++ sorting-algorithm/bubble-sort.py
@@ -1,5 +1,57 @@
-list_item = [200, 188, 290, 490, 60, 210, 43, 55, 1001, 5490, 50, 20, 594, 80, 88, 790, 500, 699, 799, 5454, 3432]
+"""
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the
+wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite
+high.
 
+In Bubble Sort algorithm,
+
+traverse from left and compare adjacent elements and the higher one is placed at right side.
+In this way, the largest element is moved to the rightmost end at first.
+This process is then continued to find the second largest and place it and so on until the data is sorted.
+
+Time Complexity: O(N2)
+Auxiliary Space: O(1)
+
+Advantages of Bubble Sort:
+    - Bubble sort is easy to understand and implement.
+    - It does not require any additional memory space.
+    - It is a stable sorting algorithm, meaning that elements with the same key value maintain their relative order in
+        the sorted output.
+
+Disadvantages of Bubble Sort:
+    - Bubble sort has a time complexity of O(N2) which makes it very slow for large data sets.
+    - Bubble sort is a comparison-based sorting algorithm, which means that it requires a comparison operator to
+        determine the relative order of elements in the input data set. It can limit the efficiency of the algorithm in
+        certain cases.
+
+Some FAQs related to Bubble Sort:
+What is the Boundary Case for Bubble sort?
+    Bubble sort takes minimum time (Order of n) when elements are already sorted. Hence, it is best to check if the
+    array is already sorted or not beforehand, to avoid O(N2) time complexity.
+
+Does sorting happen in place in Bubble sort?
+    Yes, Bubble sort performs the swapping of adjacent pairs without the use of any major data structure. Hence, Bubble
+    sort algorithm is an in-place algorithm.
+
+Is the Bubble sort algorithm stable?
+    Yes, the bubble sort algorithm is stable.
+
+Where is the Bubble sort algorithm used?
+    Due to its simplicity, bubble sort is often used to introduce the concept of a sorting algorithm. In computer
+    graphics, it is popular for its capability to detect a tiny error (like a swap of just two elements) in
+    almost-sorted arrays and fix it with just linear complexity (2n).
+
+Example: It is used in a polygon filling algorithm, where bounding lines are sorted by their x coordinate at a specific
+scan line (a line parallel to the x-axis), and with incrementing y their order changes (two elements are swapped) only
+at intersections of two lines.
+"""
+
+from random import randint
+import time
+
+list_item = [randint(a=0, b=100000) for i in range(10000)]
+print(f"Unsorted list: {list_item}")
+start_time = time.time()
 while True:
     swapped = False
     for i in range(len(list_item) - 1):
@@ -9,4 +61,7 @@ while True:
     if not swapped:
         break
 
-print(list_item)
+end_time = time.time()
+elapsed_time_ms = (end_time - start_time) * 1000
+print(f"Time it takes to sort the lists in ms: {elapsed_time_ms}")
+print(f"Sorted list: {list_item}")
--- sorting-algorithm/insertion_sort.py
+++ sorting-algorithm/insertion_sort.py
@@ -15,32 +15,68 @@ meaning it doesn't require any additional memory space beyond the original
 array.
 
 Algorithm:
+1. To achieve insertion sort, follow these steps:
+2. We have to start with second element of the array as first element in the array is assumed to be sorted.
+3. Compare second element with the first element and check if the second element is smaller than swap them.
+4. Move to the third element and compare it with the second element, then the first element and swap as necessary to put
+    it in the correct position among the first three elements.
+5. Continue this process, comparing each element with the ones before it and swapping as needed to place it in the
+    correct position among the sorted elements.
+6. Repeat until the entire array is sorted.
 
-To achieve insertion sort, follow these steps:
+Time Complexity: O(N^2)
+Auxiliary Space: O(1)
 
-We have to start with second element of the array as first element in the
-array is assumed to be sorted.
+Time Complexity of Insertion Sort
+Best case: O(n), If the list is already sorted, where n is the number of elements in the list.
+Average case: O(n2), If the list is randomly ordered
+Worst case: O(n2), If the list is in reverse order
 
-Compare second element with the first element and check if the second element
-is smaller than swap them.
+Space Complexity of Insertion Sort
+Auxiliary Space: O(1), Insertion sort requires O(1) additional space, making it a space-efficient sorting algorithm.
 
-Move to the third element and compare it with the second element, then the
-first element and swap as necessary to put it in the correct position among
-the first three elements.
+Advantages of Insertion Sort:
+    - Simple and easy to implement.
+    - Stable sorting algorithm.
+    - Efficient for small lists and nearly sorted lists.
+    - Space-efficient.
 
-Continue this process, comparing each element with the ones before it and
-swapping as needed to place it in the correct position among the sorted
-elements.
+Disadvantages of Insertion Sort:
+    - Inefficient for large lists.
+    - Not as efficient as other sorting algorithms (e.g., merge sort, quick sort) for most cases.
 
-Repeat until the entire array is sorted.
+Applications of Insertion Sort:
+    - Insertion sort is commonly used in situations where:
+    - The list is small or nearly sorted.
+    - Simplicity and stability are important.
+
+Q1. What are the Boundary Cases of the Insertion Sort algorithm?
+    - Insertion sort takes the maximum time to sort if elements are sorted in reverse order. And it takes minimum time
+        (Order of n) when elements are already sorted.
+
+Q2. What is the Algorithmic Paradigm of the Insertion Sort algorithm?
+    - The Insertion Sort algorithm follows an incremental approach.
+
+Q3. Is Insertion Sort an in-place sorting algorithm?
+    - Yes, insertion sort is an in-place sorting algorithm.
+
+Q4. Is Insertion Sort a stable algorithm?
+    - Yes, insertion sort is a stable sorting algorithm.
+
+Q5. When is the Insertion Sort algorithm used?
+    - Insertion sort is used when number of elements is small. It can also be useful when the input array is almost
+    sorted, and only a few elements are misplaced in a complete big array.
 
 Reference from geeks for geeks.
 """
 
 from random import randint
+import time
 sample_data: list[int] = [23, 1, 10, 5, 2]
-new_data: list[int] = [randint(a=0, b=10000) for i in range(25)]
+new_data: list[int] = [randint(a=0, b=100000) for i in range(10000)]
 print(f"Unsorted Array: {new_data}")
+
+start_time = time.time()
 
 for x in range(1, len(new_data)):
     i: int = x
@@ -49,4 +85,8 @@ for x in range(1, len(new_data)):
             new_data[i], new_data[i - 1] = new_data[i - 1], new_data[i]
         i -= 1
 
+end_time = time.time()
+
+elapsed_time_ms = (end_time - start_time) * 1000
+print(f"Time it takes to sort the lists in ms: {elapsed_time_ms}")
 print(f"Sorted Array: {new_data}")
